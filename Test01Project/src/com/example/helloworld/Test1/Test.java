1efcd9e4149b7edc81e2f7b498c9d0aa71c6a3fc
priyankawankhade <92149335+priyankawankhade@users.noreply.github.com>
2021-10-08 15:18:48 +0530

    Add files via upload

---
+++ Test01Project/src/com/example/helloworld/Test1/Test.java
@@ -0,0 +1,4 @@
+package com.example.helloworld.Test1;
+
+public class Test {
+}
---
+++ Test01Project/src/com/example/helloworld/helloworld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+
+import com.example.helloworld.Test1.Test;
+import com.example.helloworld.Test1.Test1;
+
+public class helloworld {
+    public static void main(String[] args) {
+        Test1 testobj = new Test1();
+        System.out.println(testobj.getName());
+
+    }
+}
